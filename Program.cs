a6d1076626635408afe39f62bd53ab1eb9e83918
karinaFlower <72452438+karinaFlower@users.noreply.github.com>
2020-10-10 13:15:09 +0900

    301

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practika301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
