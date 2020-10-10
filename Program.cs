7d167390c620dc037f4c342294fe1f28cb326407
karinaFlower <72452438+karinaFlower@users.noreply.github.com>
2020-10-10 14:49:51 +0900

    FUNAL

--- Program.cs
+++ Program.cs
@@ -10,9 +10,17 @@ namespace practika301
     {
         static void Main(string[] args)
         {
+            Class1 USER = new Class1(20, 345, "Гребенькова Карина Юрьевна",143 ,"Ваша фотография"); 
 
-
-
+            Console.WriteLine($"Возраст пользователя {USER.ReturnVozrast()} ");
+            Console.WriteLine($"Логин {USER.Returnlogin()} "); 
+            Console.WriteLine($"ФИО пользователя {USER.ReturnFio()} "); 
+            Console.WriteLine($"Пароль {USER.ReturnParol()} ");
+            Console.WriteLine($"Аватар {USER.ReturnAvatar()} ");
+            Console.WriteLine("Введите новое ФИО  _"); USER.SetFio(Console.ReadLine());
+            Console.WriteLine($"Ваше новое ФИО {USER.ReturnFio()}");
+          
+            Console.ReadLine();
 
         }
     }
